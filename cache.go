c528e7366018ef750c376fc1f22a274eb2da11da
99Kies <1290017556@qq.com>
2021-10-20 17:34:36 +0800

    update pigCache

---
+++ cache.go
@@ -0,0 +1,35 @@
+package pigCache
+
+import (
+	"github.com/99Kies/pigCache/lru"
+	"sync"
+)
+
+// 添加线程锁，防止并发处理时的线程冲突。
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
